d229764b8b0de90eda5c0fab44326845cf109178
Lionel Debroux <lionel_debroux@yahoo.fr>
2017-11-11 22:44:38 +0100

    Switch to on-calc-incompatible builds to save more space.
    Build stats:
      Program Variable Size:                    52387 Bytes
      Absolute Relocs:                          878
      Natively Emitted Relocs:                  878
      Relocs Removed by Branch Optimization:    281
      Relocs Removed by Move Optimization:      1784
      Relocs Removed by Test Optimization:      39
      Relocs Removed by Calc Optimization:      113
      Relocs Removable by F-Line Jumps:         33
      Space Saved by Range-Cutting:             4464 Bytes
    
      Program Variable Name:                    main\wwp
      Program Variable Size:                    30345 Bytes
      Absolute Relocs:                          153
      Natively Emitted Relocs:                  153
      Relocs Removed by Branch Optimization:    95
      Relocs Removed by Move Optimization:      637
      Relocs Removed by Calc Optimization:      1
      Space Saved by Range-Cutting:             1498 Bytes

--- WormsWP Sprites/Main.c
+++ WormsWP Sprites/Main.c
@@ -1,16 +1,6 @@
 // C Source File
 // Created 1/4/2004; 8:46:00 PM
 
-#define USE_TI89              // Compile for TI-89
-#define USE_TI92PLUS          // Compile for TI-92 Plus
-#define USE_V200              // Compile for V200
-
-// #define OPTIMIZE_ROM_CALLS // Use ROM Call Optimization
-
-#define MIN_AMS 100           // Compile for AMS 1.00 or higher
-
-#define SAVE_SCREEN           // Save/Restore LCD Contents
-
 #include <tigcclib.h>         // Include All Header Files
 #include "extgraph.h"
 
